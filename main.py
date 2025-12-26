8882e5e7b6dbe5b66e062a7e49baa0355baec71b
Niebieski <157232546+fiiiiiuuuu@users.noreply.github.com>
2025-12-26 17:16:32 +0300

    added arg for mime_type

--- main.py
+++ main.py
@@ -11,29 +11,40 @@ def get_arguments():
     parser.add_argument(
         '--parent_id',
         type=str,
-        default=None
+        default=None,
+        help='ID родительской папки'
     )
     parser.add_argument(
         '--folder_name',
         type=str,
-        default='Projects'
+        default='Projects',
+        help='Имя главной папки'
     )
     parser.add_argument(
         '--subfolder_name',
         type=str,
-        default='Python'
+        default='Python',
+        help='Имя подпапки'
+    )
+    parser.add_argument(
+        '--mime_type',
+        type=str,
+        default='application/octet-stream',
+        help='MimeType по умолчанию'
     )
     return parser.parse_args()
 
 
 def process_file_workflow(service, file_name, mime_type, target_folder_id):
     current_mime_type = mime_type
-    if file_name.endswith('.py'):
-        current_mime_type = 'text/x-python'
-    elif file_name.endswith('.png'):
-        current_mime_type = 'image/png'
-    elif file_name.endswith('.jpg') or file_name.endswith('.jpeg'):
-        current_mime_type = 'image/jpeg'
+
+    if mime_type == 'application/octet-stream':
+        if file_name.endswith('.py'):
+            current_mime_type = 'text/x-python'
+        elif file_name.endswith('.png'):
+            current_mime_type = 'image/png'
+        elif file_name.endswith('.jpg') or file_name.endswith('.jpeg'):
+            current_mime_type = 'image/jpeg'
 
     uploaded_file_id = upload_file(service, file_name, current_mime_type, target_folder_id)
     web_view_link = get_sharing_link(service, uploaded_file_id)
@@ -54,7 +65,7 @@ def main():
     projects_folder_id = create_folder(service, args.folder_name, args.parent_id)
     target_folder_id = create_folder(service, args.subfolder_name, projects_folder_id)
 
-    process_file_workflow(service, 'main.py', 'text/x-python', target_folder_id)
+    process_file_workflow(service, 'main.py', args.mime_type, target_folder_id)
 
 
 if __name__ == '__main__':
